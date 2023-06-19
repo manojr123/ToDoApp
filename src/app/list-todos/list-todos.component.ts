4e7d4deeb90ed567a10559691627e91a7c4b6b5e
manojr123 <manojraghavan2008@yahoo.co.in>
2023-06-19 14:06:20 +0530

    Best Practice - create a Todo class

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id : number,
+    public description : string,
+    public done :boolean,
+    public targetDate : Date,
+  ) {
+
+  }
+}
+
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
+
+
+
 export class ListTodosComponent {
 
   todos= [
-    {id: 1, description :'Learn to dance'},
-    {id: 2, description :'Become an expert in India'},
-    {id: 3, description :'Visit USA'},
+    new Todo(1, 'Learn to dance', false, new Date()),
+    new Todo(2, 'Become an expert in Angular', false, new Date()),
+    new Todo(3, 'Visit', false, new Date()),
   ];
 
   // todo = {
